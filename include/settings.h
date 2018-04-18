93024c35ba4a511abf7df5a52e4cb392fa6a80f3
Nicolas Senecal <nicolassenecal83@gmail.com>
2018-04-18 15:14:27 +0200

    Ajout d'un fichier de paramétrage

--- include/settings.h
+++ include/settings.h
@@ -9,9 +9,11 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-
 #define RGBA_EXTENSION (const char *[3]){"png","gif","bpm"} /* Les différentes extensions des images en RGBA */
 #define RGBA_EXTENSION_SIZE (int) 3 /* Taille du tableau ci-dessus */
+
+/* DEBUG */
+#define SHOW_BOUNDING_BOX 1
 
 
 #endif
