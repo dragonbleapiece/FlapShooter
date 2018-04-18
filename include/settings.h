63929e047b45736de0ec467c548388a89dbd9f52
Nicolas Senecal <nicolassenecal83@gmail.com>
2018-04-18 16:17:43 +0200

    Ajout de parametres

--- include/settings.h
+++ include/settings.h
@@ -12,8 +12,10 @@
 #define RGBA_EXTENSION (const char *[3]){"png","gif","bpm"} /* Les différentes extensions des images en RGBA */
 #define RGBA_EXTENSION_SIZE (int) 3 /* Taille du tableau ci-dessus */
 
-/* DEBUG */
-#define SHOW_BOUNDING_BOX 1
+#define CERCLE_LIGNES 50 /* Nombre de polygones d'un cercle */
 
+/* DEBUG */
+#define SHOW_BOUNDING_BOX 1 /* Afficher les bounding box en violet sur les entités */
+#define BOUNDING_BOX_COLOR 148,0,211,100 /* Couleur RGBA des bounding box */
 
 #endif
